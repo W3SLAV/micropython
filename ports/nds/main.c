9284aa138b550c4be763d403e801248988601cc4
W3SLAV/gt4ous <49104904+W3SLAV@users.noreply.github.com>
2024-06-09 12:45:12 -0500

    add 2 screen support
    Signed-off-by: W3SLAV/gt4ous <49104904+W3SLAV@users.noreply.github.com>

--- ports/nds/main.c
+++ ports/nds/main.c
@@ -54,7 +54,19 @@ static void do_str(const char *src, mp_parse_input_kind_t input_kind) {
 int main(void) {
     //consoleDemoInit();
     // Initialize the console for printing to the screen.
-    consoleDemoInit();
+    
+    PrintConsole topScreen;
+    PrintConsole bottomScreen;
+
+    videoSetMode(MODE_0_2D);
+    videoSetModeSub(MODE_0_2D);
+
+    vramSetBankA(VRAM_A_MAIN_BG);
+    vramSetBankC(VRAM_C_SUB_BG);
+
+    consoleInit(&topScreen, 3,BgType_Text4bpp, BgSize_T_256x256, 31, 0, true, true);
+    consoleInit(&bottomScreen, 3,BgType_Text4bpp, BgSize_T_256x256, 31, 0, false, true);
+    consoleSelect(&topScreen);
 
 
     mp_init();
